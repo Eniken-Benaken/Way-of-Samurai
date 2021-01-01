70d94450008c1e865a97978430b577f9c1386f8f
Eniken-Benaken <strippedstar@gmail.com>
2021-01-01 21:47:50 +0300

    Made first step to social net, added some styles

--- way-of-samurai/src/App.js
+++ way-of-samurai/src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
-import Header from './Header';
-import Techs from './Techs';
 
 const App = () => {
   return (
-    <div className="App">
-			<Header />
-      <Techs />
+    <div className="app-wrapper">
+			<header>
+				<img src='https://dummyimage.com/200x120/00eaff/ffffff&text=Company+Ico'></img>
+			</header>
+			<nav>
+				<div><a href="#">Profile</a></div>
+				<div><a href="#">Messages</a></div>
+				<div><a href="#">News</a></div>
+				<div><a href="#">Music</a></div>
+				<div><a href="#">Settings</a></div>
+			</nav> 
+			<main>
+				<div>
+					<img src="https://source.unsplash.com/1200x200/?newyear" alt=""/>
+				</div>
+				<div>ava + description</div>
+				<div>
+					My posts
+					<div>New Post</div>
+					<div>Post1</div>
+					<div>Post2</div>
+				</div>
+				
+			</main>
     </div>
   );
 };
