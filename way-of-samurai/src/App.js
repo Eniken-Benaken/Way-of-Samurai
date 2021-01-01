a8e9e58c4827caf2f782787614a4f681e6220936
Eniken-Benaken <strippedstar@gmail.com>
2021-01-01 10:48:01 +0300

    Moved components into different files and imported to app

--- way-of-samurai/src/App.js
+++ way-of-samurai/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Techs from './Techs';
 
 const App = () => {
   return (
@@ -8,31 +9,6 @@ const App = () => {
       <Techs />
     </div>
   );
-}
-
-const Techs = () => {
-	return (
-		<>
-			<ul>
-				<li>html</li>
-				<li>css</li>
-				<li>js</li>
-				<li>react</li>
-			</ul>
-		</>
-	)
-}
-
-const Header = () => {
-	return (
-		<>
-			<a href="/">Profile</a>
-			<a href="/">Feed</a>
-			<a href="/">Messages</a>
-			<a href="/">Settings</a>
-		</>
-	)
-}
-
+};
 
 export default App;
---
+++ way-of-samurai/src/Techs.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Techs = (props) => {
+	return(
+		<>
+			<ul>
+				<li>html</li>
+				<li>css</li>
+				<li>js</li>
+				<li>react</li>
+			</ul>
+		</>
+	);
+}
+
+
+export default Techs;
