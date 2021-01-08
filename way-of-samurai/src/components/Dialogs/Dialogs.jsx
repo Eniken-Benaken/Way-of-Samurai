98acbf50d0a24836bde08e41af2fae5ddec15f0b
Eniken-Benaken <strippedstar@gmail.com>
2021-01-08 20:46:30 +0300

    first attempt to split data and logic

--- way-of-samurai/src/components/Dialogs/Dialogs.jsx
+++ way-of-samurai/src/components/Dialogs/Dialogs.jsx
@@ -5,23 +5,42 @@ import Message from './Message/Message';
 
 
 const Dialogs = (props) => {
+	let dialogs_data = [
+		{ id: 'pavlo' , dialog_name:"Pavlo"  },
+		{ id: 'meyrin', dialog_name:"Meyrin" },
+		{ id: 'katya' , dialog_name:"Katya"  },
+		{ id: 'bruno' , dialog_name:"Bruno"  },
+		{ id: 'marko' , dialog_name:"Marko"  },
+		{ id: 'roman' , dialog_name:"Roman"  }
+	]
+
+	let messages_data = [
+		{ id: 1, message: "We need to get food." },
+		{ id: 2, message: "This is so wrong to steal food from others like us!" },
+		{ id: 3, message: "I would eat anything!" },
+		{ id: 4, message: "I haven't slept for a long time" },
+		{ id: 5, message: "Smoke is soothing me." },
+		{ id: 6, message: "I hope we will be able to warm up this shelter" }
+	]
+
+
 	return (
 		<div className={s.dialogs_wrapper}>
 			<div className={s.dialog_items}>
-				<Dialog id='pavlo' dialog_name="Pavlo" />
-				<Dialog id='meyrin' dialog_name="Meyrin" />
-				<Dialog id='katya' dialog_name="Katya" />
-				<Dialog id='bruno' dialog_name="Bruno" />
-				<Dialog id='marko' dialog_name="Marko" />
-				<Dialog id='roman' dialog_name="Roman" />
+				<Dialog id={dialogs_data[0].id} dialog_name={dialogs_data[0].dialog_name} />
+				<Dialog id={dialogs_data[1].id} dialog_name={dialogs_data[1].dialog_name} />
+				<Dialog id={dialogs_data[2].id} dialog_name={dialogs_data[2].dialog_name} />
+				<Dialog id={dialogs_data[3].id} dialog_name={dialogs_data[3].dialog_name} />
+				<Dialog id={dialogs_data[4].id} dialog_name={dialogs_data[4].dialog_name} />
+				<Dialog id={dialogs_data[5].id} dialog_name={dialogs_data[5].dialog_name} />
 			</div>
 			<div className={s.messages}>
-				<Message message='We need to get food.' />
-				<Message message='This is so wrong to steal food from others like us!' />
-				<Message message='I would eat anything!' />
-				<Message message="I haven't slept for a long time" />
-				<Message message='Smoke is soothing me.' />
-				<Message message='I hope we will be able to warm up this shelter' />
+				<Message id={messages_data[0].id} message={messages_data[0].message} />
+				<Message id={messages_data[1].id} message={messages_data[1].message} />
+				<Message id={messages_data[2].id} message={messages_data[2].message} />
+				<Message id={messages_data[3].id} message={messages_data[3].message} />
+				<Message id={messages_data[4].id} message={messages_data[4].message} />
+				<Message id={messages_data[5].id} message={messages_data[5].message} />
 			</div>
 		</div>
 	);
--- way-of-samurai/src/components/Profile/Posts/MyPosts.jsx
+++ way-of-samurai/src/components/Profile/Posts/MyPosts.jsx
@@ -3,6 +3,16 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = (props) => {
+	let dialogs_data = [
+		{ 
+			id: 1, 
+			author: 'Dem Pigeon', 
+			post_content: 'Hey, My name is Dem Pigeon',
+			likes: 13
+		},
+		{ id: '2', author: 'Dem Pigeon', post_content: 'This is my first post' }
+	]
+
 	return(
 		<div className={s.posts}>
 			<div className={s.new_post}>
@@ -10,8 +20,19 @@ const MyPosts = (props) => {
 				<button>Add New</button>
 				<button>Upload Image</button>
 			</div>
-			<Post author='Dem Pigeon' post_content="Hey, My name is Dem Pigeon" likes="13" />
-			<Post author='Dem Pigeon' post_content="This is my first post" likes="2" />
+			<Post 
+				id={dialogs_data[0].id}
+				author={dialogs_data[0].author}
+				post_content={dialogs_data[0].post_content}
+				likes={dialogs_data[0].likes} 
+			/>
+			<Post 
+				id={dialogs_data[1].id}
+				author={dialogs_data[1].author}
+				post_content={dialogs_data[1].post_content}
+				likes={dialogs_data[1].likes} 
+			/>
+			
 		</div>
 	);
 }
