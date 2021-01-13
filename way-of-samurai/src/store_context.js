2ffbb86beb681308255bbea7c859c73f0d9b9123
Eniken-Benaken <strippedstar@gmail.com>
2021-01-13 21:28:25 +0300

    a little practice for props.children

--- way-of-samurai/src/store_context.js
+++ way-of-samurai/src/store_context.js
@@ -2,3 +2,11 @@ import React from 'react';
 
 export const MyContext = React.createContext(null);
 MyContext.displayName = 'Store_Context';
+
+export const Provider = (props) => {
+	return (
+		<MyContext.Provider value={props.store}>
+			{props.children}
+		</MyContext.Provider>
+	)
+}
