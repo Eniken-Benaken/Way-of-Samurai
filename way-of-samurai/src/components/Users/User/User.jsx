c2a86980abb8fc2c428f26a4b4f75c4a24e9c50d
Eniken-Benaken <strippedstar@gmail.com>
2021-01-17 11:42:48 +0300

    added styles to users page

--- way-of-samurai/src/components/Users/User/User.jsx
+++ way-of-samurai/src/components/Users/User/User.jsx
@@ -11,25 +11,29 @@ const User = (props) => {
 	};
 
 	const button = props.is_followed ?
-			<button onClick={() => unfollowUser(props.user_id)}>Unfollow</button> :
-			<button onClick={() => followUser(props.user_id)}>Follow</button>;
-	
-	return(
+		<button className={s.followed} onClick={() => unfollowUser(props.user_id)}>Unfollow</button> :
+		<button onClick={() => followUser(props.user_id)}>Follow</button>;
+
+	return (
 		<div className={s.item} id={props.user_id}>
-			<img src={props.user_avatar} alt="avatar"/>
-			<div className={s.post_content_wrapper}>
-				<div className={s.author}>
-					<a href={props.user_id}>{props.user_name}</a>
-				</div>
-				<div className={s.post_content}>{props.user_status_message}</div>
-			</div>
-			<div className={s.action_buttons}>
-				<div className={s.likes}>{props.user_city}</div>			
-				<div className={s.add_comment}>{props.user_country}</div>
+			<div className={s.avatar_follow}>
+				<img src={props.user_avatar} alt="avatar" />
 				{button}
 			</div>
+			<div className={s.user_info_wrapper}>
+				<div className={s.user_info}>
+					<div className={s.user_name}>
+						<a href={props.user_id}>{props.user_name}</a>
+					</div>
+					<div className={s.user_status_message}>
+						"{props.user_status_message}"
+					</div>
+				</div>
+				<div className={s.user_location_wrapper}>
+					<div className={s.user_location}>{props.user_country}, {props.user_city}</div>
+				</div>
+			</div>
 		</div>
-		/* <div className={s.item}>{props.post_contents}</div> */
 	);
 }
 
